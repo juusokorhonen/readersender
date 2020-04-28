d3a29eaada25211c845d5d25f809cd5b99ff9194
Juuso Korhonen <juuso@turqoosi.net>
2020-04-28 11:26:51 +0300

    MOD: Drop support for python 2.x

--- readersender/__init__.py
+++ readersender/__init__.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf8
 
+import sys
+assert sys.version_info.major == 3
+
 from .readersender import ReaderSender
 from .reader import Sender
 from .sender import Reader
